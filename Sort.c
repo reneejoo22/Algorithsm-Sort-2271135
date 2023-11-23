15eaa38099347d311cdb1787da2cccec1adfe3b9
reneejoo22 <renee.joo03@gmail.com>
2023-11-24 02:28:36 +0900

    쉘정렬

--- Sort.c
+++ Sort.c
@@ -1,37 +1,60 @@
-//삽입정렬
+//쉘정렬_삽입 정렬의 더 나은 버전
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_SIZE 4
+#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))	//x,y 값을 교환
 
 //int list[MAX_SIZE];
 int n;
 
-void insert_sort(int list[], int n) {	//삽입정렬 오름차순
+void inc_insertion_sort(int list[], int first, int last, int gap) {	//내림차순
 
-	int i, j, key;	//unsorted 영역 내의 위치, sorted 영역 내의 위치, sorted
+	int i, j, key;
 
-	for (i = 1; i < n; i++) {	//unsorted 영역의 가장 초기값
-		key = list[i];	//key에 저장
-
-		for (j = i - 1; j >= 0 && list[j] > key; j--)	//sorted 영역의 초기값, 키값이 더 작을시
-			list[j + 1] = list[j];	//sorted 초기값이 오른쪽으로 옮겨감
-		list[j + 1] = key;	//아닐시 그 자리에 그대로 있음
+	for (i = first + gap; i <= last; i = i + gap) {	//반토막 낸 내에서
+		key = list[i];
+		for (j = i - gap; j >= first && list[j] > key; j = j - gap)	// 키값이 더 작을시
+			list[j + gap] = list[j];	//sorted 초기값이 오른쪽으로 옮겨감
+		list[j + gap] = key;	//아닐시 그 자리에 그대로 있음
 	}
-	
+
 }
 
-void insert_sort2(int list[], int n) {	//삽입정렬 내림차순
+void inc_insertion_sort2(int list[], int first, int last, int gap) {	//오름차순
 
-	int i, j, key;	//unsorted 영역 내의 위치, sorted 영역 내의 위치, sorted
+	int i, j, key;
 
-	for (i = 1; i < n; i++) {	//unsorted 영역의 가장 초기값
-		key = list[i];	//key에 저장
+	for (i = first + gap; i <= last; i = i + gap) {	//반토막 낸 내에서
+		key = list[i];
+		for (j = i - gap; j >= first && list[j] < key; j = j - gap)	// 키값이 더 클시
+			list[j + gap] = list[j];	//sorted 초기값이 오른쪽으로 옮겨감
+		list[j + gap] = key;	//아닐시 그 자리에 그대로 있음
+	}
 
-		for (j = i - 1; j >= 0 && list[j] < key; j--)	//sorted 영역의 초기값, 키값이 더 클시
-			list[j + 1] = list[j];	//sorted 초기값이 오른쪽으로 옮겨감
-		list[j + 1] = key;	//아닐시 그 자리에 그대로 있음
+}
+
+void shell_sort(int list[], int n) {
+
+	int i, gap;	
+
+	for (gap = n / 2; gap > 0; gap = gap /2) {	//반토막 낸 값 계속 보내줘야됨
+		if ((gap % 2) == 0) gap++;
+		for (i = 0; i < gap; i++)
+			inc_insertion_sort(list, i, n - 1, gap);
+	}
+
+}
+
+void shell_sort2(int list[], int n) {
+
+	int i, gap;
+
+	for (gap = n / 2; gap > 0; gap = gap / 2) {	//반토막 낸 값 계속 보내줘야됨
+		if ((gap % 2) == 0) gap++;
+		for (i = 0; i < gap; i++)
+			inc_insertion_sort2(list, i, n - 1, gap);
 	}
 }
 
@@ -49,16 +72,16 @@ int main(void) {
 
 	int list[MAX_SIZE] = { 7,4,6,5 };
 
-	insert_sort(list, n);  //선택정렬 오름차순 시작
-
-	printf("삽입 정렬 오름차순 결과\n");
+	shell_sort(list, n);  //쉘정렬 오름차순 시작
+	
+	printf("쉘정렬 오름차순 결과\n");
 	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
 		printf("%d ", list[i]);
+	
 	printf("\n\n******************************\n\n");
 
-	insert_sort2(list, n);  //선택정렬 내림차순 시작
-
-	printf("삽입 정렬 내림차순 결과\n");
+	printf("쉘정렬 오름차순 결과\n");
+	shell_sort2(list, n);  //쉘정렬 오름차순 시작
 	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
 		printf("%d ", list[i]);
 	printf("\n");
@@ -67,7 +90,6 @@ int main(void) {
 }
 
 /*
-	1. sorted 와 unsorted 나눔
-	2. sorted 바로 다음의 값을 key에 저장
-	3. key 보다 sorted 값이 작으면 왼쪽으로,
+	1.가장 작은것 찾고 위치 저장
+	2. 해당 위치랑 제일 첫번째 자리와 swap
 */
