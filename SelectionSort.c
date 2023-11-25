c874085b7d2c5a60a244fe0f17ab439984dbf596
reneejoo22 <renee.joo03@gmail.com>
2023-11-26 03:09:29 +0900

    퀵 정렬, t(퀵정렬, 라이브러리 함수 사용)

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,90 +1,144 @@
-//선택정렬
+//퀵 정렬, t(퀵정렬, 라이브러리 함수 사용)
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #define MAX_SIZE 4
-#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))	//x,y 값을 교환
+#define SWAP(x, y, temp) ((temp) = (x), (x) = (y), (y) = (temp)) // x, y 값을 교환
 
-//int list[MAX_SIZE];
+int list[MAX_SIZE];
 int n;
 
-void selection_sort(int list[], int n) {	//선택정렬 오름차순
-	
-	int i, j, least, temp;	//갱신되는 초기 위치, 갱신되는 최소 찾는 위치, 최소의 위치, 저장
-
-	int num = 0;
-
-	printf("선택정렬 오름차순 시작\n\n");
-
-	for (i = 0; i < n - 1; i++) {	//초기 자리 제외하고 찾기
-		least = i;	//i에 초기 자리 저장
-		for (j = i + 1; j < n; j++)	//초기 자리 제하고 최솟값 탐색
-			if (list[j] < list[least])least = j;	// 초기보다 작은 값 찾으면 해당 값 위치 저장
-		SWAP(list[i], list[least], temp);	//바꾸기
-
-
-		printf("====cycle %d====\n", ++num);
-		for (int q = 0; q < n; q++)	//정렬된 것 순서대로 출력
-			printf("%d ", list[q]);
-		printf("\n");
-	}
-	printf("\n");
+int compare(const void* x, const void* y) {
+    return (*(int*)x - *(int*)y); // 오름차순
+}
+int compare2(const void* x, const void* y) {
+    return (*(int*)y - *(int*)x); // 내림차순
 }
 
-void selection_sort2(int list[], int n) {	//선택정렬 내림차순
+void print_list(int list[], int n) {
 
-	int i, j, least, temp;	//갱신되는 초기 위치, 갱신되는 최대 찾는 위치, 최대의 위치, 저장
-	
-	int num = 0;
-
-	printf("선택정렬 내림차순 시작\n\n");
-	
-	for (i = 0; i < n - 1; i++) {	//초기 자리 제외하고 찾기
-		least = i;	//i에 초기 자리 저장
-		for (j = i + 1; j < n; j++)	//초기 자리 제하고 최댓값 탐색
-			if (list[j] > list[least])least = j;	// 초기보다 큰 값 찾으면 해당 값 위치 저장
-		SWAP(list[i], list[least], temp);	//바꾸기
-		
-		printf("====cycle %d====\n", ++num);
-		for (int q = 0; q < n; q++)	//정렬된 것 순서대로 출력
-			printf("%d ", list[q]);
-		printf("\n");
-	}
-	printf("\n");
+    for (int i = 0; i < n; i++) // 정렬된 것 순서대로 출력
+        printf("%d ", list[i]);
+    printf("\n");
 }
+
+int partition(int list[], int left, int right) {    //오름
+    
+    int pivot, temp;
+    int low, high;
+
+    low = left;
+    high = right + 1;
+    pivot = list[left];
+
+    do {
+        do
+            low++;
+        while (list[low] < pivot);  //low가 더 작은 경우 탐색 계속 진행
+        
+        do
+            high--;
+        while (list[high] > pivot); //high가 더 큰 경우 탐색 계속 진행
+
+        if (low < high) {
+            SWAP(list[low], list[high], temp);   //와일문에 반할시 둘의 값 바꿈
+
+
+            print_list(list, MAX_SIZE);
+        }
+    
+    } while (low < high);   //둘이 교차하면 탐색 멈춤
+
+    SWAP(list[left], list[high], temp);
+    
+    print_list(list, MAX_SIZE);
+
+    return high;
+}
+
+int partition2(int list[], int left, int right) {   //내림
+
+    int pivot, temp;
+    int low, high;
+
+    low = left;
+    high = right + 1;
+    pivot = list[left];
+
+    do {
+        do
+            low++;
+        while (list[low] > pivot);  //low가 더 작은 경우 탐색 계속 진행
+
+        do
+            high--;
+        while (list[high] < pivot); //high가 더 큰 경우 탐색 계속 진행
+
+        if (low < high) {
+            SWAP(list[low], list[high], temp);   //와일문에 반할시 둘의 값 바꿈
+           
+            print_list(list, MAX_SIZE);
+        }
+    } while (low < high);   //둘이 교차하면 탐색 멈춤
+
+    SWAP(list[left], list[high], temp);
+    print_list(list, MAX_SIZE);
+
+    return high;
+}
+
+
+void quick_sort(int list[], int left, int right, int choice) {
+
+    if (left < right) {
+        if (choice == 0) {
+            int q = partition(list, left, right, choice);
+            quick_sort(list, left, q - 1, choice);  //왼쪽만 정렬진행
+            quick_sort(list, q + 1, right, choice); //른쪽만 정렬진행
+        }
+        else {
+            int q = partition2(list, left, right, choice);
+            quick_sort(list, left, q - 1, choice);  //왼쪽만 정렬진행
+            quick_sort(list, q + 1, right, choice); //른쪽만 정렬진행
+
+        }
+    }
+}
+
+
 
 int main(void) {
-	
-	int i;
-	n = MAX_SIZE;
 
-	/*srand(time(NULL));
-	for (i = 0; i < n; i++)	//정해진 값만큼 난수 생성
-		list[i] = rand() % 100; //0~99
-	*/
+    int i;
+    n = MAX_SIZE;
+    /*
+    srand(time(NULL));
+    for (i = 0; i < n; i++) // 정해진 값만큼 난수 생성
+        list[i] = rand() % 100; // 0~99
+        */
 
-	int list[MAX_SIZE] = { 7,4,6,5 };
+    int list[MAX_SIZE] = { 6, 7, 4, 5 };
 
-	selection_sort(list, n);  //선택정렬 오름차순 시작
-	
-	printf("선택 정렬 오름차순 결과\n");
-	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
-		printf("%d ",list[i]);
-	printf("\n\n******************************\n\n");
+    quick_sort(list, 0, n - 1, 0); // 선택정렬 오름차순(0) 시작
+
+    printf("퀵 정렬 오름차순 결과\n");
+    print_list(list, n);
+    printf("\n\n******************************\n\n");
+
+    quick_sort(list, 0, n - 1, 1); // 선택정렬 내림차순(1) 시작
+    printf("퀵 정렬 내림차순 결과\n");
+    print_list(list ,n);
+
+    printf("\n[오름]퀵 라이버리 함수 사용 결과\n");
+    qsort(list, MAX_SIZE, sizeof(int),compare);
+    print_list(list, n);
+
+    printf("\n[내림]퀵 라이버리 함수 사용 결과\n");
+    qsort(list, MAX_SIZE, sizeof(int), compare2);
+    print_list(list, n);
 
 
-	selection_sort2(list, n);  //선택정렬 내림차순 시작
-
-	printf("선택 정렬 내림차순 결과\n");
-	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
-		printf("%d ", list[i]);
-	printf("\n");
-
-	return 0;
+    return 0;
 }
-
-/*
-	1.가장 작은것 찾고 위치 저장
-	2. 해당 위치랑 제일 첫번째 자리와 swap
-*/
