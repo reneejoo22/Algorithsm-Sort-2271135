4e493a72abbe6fa9bf9d3746501037fe1245c050
reneejoo22 <renee.joo03@gmail.com>
2023-11-27 19:58:25 +0900

    기 수 정렬

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,86 +1,104 @@
-//선택정렬
+//기수정렬
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 4
-#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))	//x,y 값을 교환
+#define MAX_QUEUE_SIZE 100
+typedef int element;
 
-//int list[MAX_SIZE];
-int n;
+typedef struct {
+	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+}QueueType;
 
-void selection_sort(int list[], int n) {	//선택정렬 오름차순
-	
-	int i, j, least, temp;	//갱신되는 초기 위치, 갱신되는 최소 찾는 위치, 최소의 위치, 저장
-
-	int num = 0;
-
-	printf("선택정렬 오름차순 시작\n\n");
-
-	for (i = 0; i < n - 1; i++) {	//초기 자리 제외하고 찾기
-		least = i;	//i에 초기 자리 저장
-		for (j = i + 1; j < n; j++)	//초기 자리 제하고 최솟값 탐색
-			if (list[j] < list[least])least = j;	// 초기보다 작은 값 찾으면 해당 값 위치 저장
-		SWAP(list[i], list[least], temp);	//바꾸기
-
-
-		printf("====cycle %d====\n", ++num);
-		for (int q = 0; q < n; q++)	//정렬된 것 순서대로 출력
-			printf("%d ", list[q]);
-		printf("\n");
-	}
-	printf("\n");
+void error(char* message) {
+	fprintf(stderr, "%s\n", message);
+	exit(1);
 }
 
-void selection_sort2(int list[], int n) {	//선택정렬 내림차순
+void init_queue(QueueType* q) {
 
-	int i, j, least, temp;	//갱신되는 초기 위치, 갱신되는 최대 찾는 위치, 최대의 위치, 저장
-	
-	int num = 0;
-
-	printf("선택정렬 내림차순 시작\n\n");
-	
-	for (i = 0; i < n - 1; i++) {	//초기 자리 제외하고 찾기
-		least = i;	//i에 초기 자리 저장
-		for (j = i + 1; j < n; j++)	//초기 자리 제하고 최댓값 탐색
-			if (list[j] > list[least])least = j;	// 초기보다 큰 값 찾으면 해당 값 위치 저장
-		SWAP(list[i], list[least], temp);	//바꾸기
-		
-		printf("====cycle %d====\n", ++num);
-		for (int q = 0; q < n; q++)	//정렬된 것 순서대로 출력
-			printf("%d ", list[q]);
-		printf("\n");
-	}
-	printf("\n");
+	q->front = q->rear = 0;
 }
+
+int is_empty(QueueType* q) {
+	
+	return (q->front == q->rear);
+}
+
+int is_full(QueueType* q) {
+
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+void enqueue(QueueType* q, element item) {
+
+	if (is_full(q))
+		error("큐가 포화 상태");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+int dequeue(QueueType* q) {
+
+	if (is_empty(q))
+		error("큐가 공백 상태");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+#define BUCKETS 10
+#define DIGITS 4
+
+void radix_sort(int list[], int n) {
+	
+	int i, b, d, factor = 1;
+	QueueType queues[BUCKETS];
+
+	for (b = 0; b < BUCKETS; b++) init_queue(&queues[b]);
+
+	for (d = 0; d < DIGITS; d++) {
+		for (i = 0; i < n; i++)
+			enqueue(&queues[(list[i] / factor) % 10], list[i]);
+	}
+
+	for (b = i = 0; b < BUCKETS; b++) {
+		while (!is_empty(&queues[b]))
+			list[i++] = dequeue(&queues[b]);
+		factor *= 10;
+	}
+}
+
+#define SIZE 5
 
 int main(void) {
 	
-	int i;
-	n = MAX_SIZE;
+	int list[SIZE] = {63, 21, 47, 52, 33};
+
+	//n = MAX_SIZE;
 
 	/*srand(time(NULL));
 	for (i = 0; i < n; i++)	//정해진 값만큼 난수 생성
 		list[i] = rand() % 100; //0~99
 	*/
 
-	int list[MAX_SIZE] = { 7,4,6,5 };
 
-	selection_sort(list, n);  //선택정렬 오름차순 시작
+
+	radix_sort(list, SIZE);  //선택정렬 오름차순 시작
 	
 	printf("선택 정렬 오름차순 결과\n");
-	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
+	for (int i = 0; i < SIZE; i++)	//정렬된 것 순서대로 출력
 		printf("%d ",list[i]);
 	printf("\n\n******************************\n\n");
 
-
+	/*
 	selection_sort2(list, n);  //선택정렬 내림차순 시작
 
 	printf("선택 정렬 내림차순 결과\n");
 	for (i = 0; i < n; i++)	//정렬된 것 순서대로 출력
 		printf("%d ", list[i]);
 	printf("\n");
-
+	*/
 	return 0;
 }
 
